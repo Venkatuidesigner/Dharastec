6da9419746096dcefc3541d77e54d88414aa7f8b
Venkat <venkatuidesigner@gmail.com>
2025-10-11 12:06:56 +0530

    Pages UI Creation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { AboutUs } from './pages/about-us/about-us';
+import { ContactUs } from './pages/contact-us/contact-us';
+import { Services } from './pages/services/services';
+import { Portfolio } from './pages/portfolio/portfolio';
 
 export const routes: Routes = [
     { path: '', component: Home },
     { path: 'home', component: Home },
     { path: 'about', component: AboutUs },
+    { path: 'contact-us', component: ContactUs },
+    { path: 'services', component: Services },
+    { path: 'portfolio', component: Portfolio },
 ];
