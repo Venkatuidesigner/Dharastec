2a3c546517ed2c2a38b7b9d61de4c6f9e8f83b04
Venkat <venkatuidesigner@gmail.com>
2025-10-11 20:24:14 +0530

    Contact Us page UI added

--- src/app/pages/contact-us/contact-us.ts
+++ src/app/pages/contact-us/contact-us.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { dummy_img, dummy_img_1} from '../../shared/globals'
 
 @Component({
   selector: 'app-contact-us',
@@ -7,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrl: './contact-us.scss'
 })
 export class ContactUs {
-
+  dummy_img_1= dummy_img_1;
+  dummy_img= dummy_img;
+  ourServices = [
+    { img: dummy_img, icon: 'fa-user-tie', title: 'Visit Us Daily', descrption: 'End-to-end technology solutions that include development, support, and consulting to keep your business running smoothly.' },
+    { img: dummy_img, icon: 'fa-mobile-screen-button', title: 'Call US', descrption: 'From idea to launch, we build innovative digital products tailored to your business goals.' },
+    { img: dummy_img, icon: 'fa-brands fa-square-whatsapp', title: 'Whatsapp', descrption: 'Modern, user-friendly designs that blend creativity with functionality to enhance user experience.' },
+    { img: dummy_img, icon: 'fa-envelope', title: 'Email Us', descrption: 'Hire us your way — choose a complete project package or hourly-based hiring that fits your needs.' },
+  ]
 }
