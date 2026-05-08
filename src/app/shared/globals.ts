795fac1c5dd434bfd080a15bb045e7bf3a96dc14
Venkat <venkatuidesigner@gmail.com>
2026-05-08 18:33:23 +0530

    UI Update

--- src/app/shared/globals.ts
+++ src/app/shared/globals.ts
@@ -1,14 +1,14 @@
 // Main logo
-export const logo = '../../../images/logo/dharstec-logo.svg';
+export const logo = 'images/logo/dharstec-logo.svg';
 
 //About Us
-export const about_us = '../../../images/about-us.png';
-export const code = '../../../images/code.jpg';
-export const dummy_img = '../../../images/dummy_img.jpg';
+export const about_us = 'images/about-us.png';
+export const code = 'images/code.jpg';
+export const dummy_img = 'images/dummy_img.jpg';
 export const dummy_img_1 = 'https://cdn.pixabay.com/photo/2019/03/12/20/28/software-4051778_1280.jpg';
-export const user = '../../../images/user.png';
+export const user = 'images/user.png';
 export const dummy_img_2 = 'https://i.pinimg.com/1200x/9d/5f/ea/9d5fea438a7e7f9d4fd63d0a6012c114.jpg';
 
 //Contact us
-export const contactus = '../../../images/contactus.svg';
-export const wcu = '../../../images/why-chooseus.jpg';
+export const contactus = 'images/contactus.svg';
+export const wcu = 'images/why-chooseus.jpg';
